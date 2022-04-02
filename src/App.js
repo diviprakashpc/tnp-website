4dde7a3c4eb270a37c85b58f2dadaa11421e4729
Ayush Tiwari <ayushtiwari820@gmail.com>
2022-04-02 16:17:43 +0530

    home completed

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './Components/Navbar/Navbar';
 import Home from './Components/Home/Home';
+import About from './Components/About/About';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Home />
+      <About />
     </div>
   );
 }
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,11 +1,35 @@
 import React from 'react'
-
+import './Home.css';
 function Home() {
-  return (
-    <div className='home' id='home'>
-        <img src="images/college1.jpg" className='home-image' alt='College-image'></img>
-    </div>
-  )
+    return (
+        <>
+        <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="images/college1.jpg" class="d-block w-100" alt="..." />
+                </div>
+                {/* <div class="carousel-item">
+                    <img src="..." class="d-block w-100" alt="..." />
+                </div>
+                <div class="carousel-item">
+                    <img src="..." class="d-block w-100" alt="..." />
+                </div> */}
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+        <div className='home-content'>
+            <h1>Training and Placement Cell</h1>
+            <h2>IIIT BHOPAL</h2>
+        </div>
+        </>
+    )
 }
 
 export default Home
