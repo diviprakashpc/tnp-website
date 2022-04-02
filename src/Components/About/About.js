dc23816447ffbb88a134b9265b340a528d5f8946
Ayush Tiwari <ayushtiwari820@gmail.com>
2022-04-02 16:44:56 +0530

    added about us

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,8 +1,12 @@
 import React from 'react'
-// import './About.css';
+import './About.css';
 const About = () => {
   return (
-    <div>About</div>
+    <div className='about'>
+        <h2>About Us</h2>
+        <p>INDIAN INSTITUTE OF INFORMATION TECHNOLOGY BHOPAL (IIIT-Bhopal), is one of the Indian Institutes of Information Technology (PPP) of National Importance in the country, set up with the objective of making available facilities for higher education, research and training in various fields of Information Technology. To address the challenges faced by the Indian IT industry and growth of the domestic IT market, the Ministry of Education (MoE), Government of India intends to establish twenty Indian Institutes of Information Technology (IIIT), on a Not-for-profit Public Private Partnership (N-PPP) basis. The partners in setting up the IIITs would be the MoE, Governments of the respective States where each IIIT will be established, and the industry. On the same line INDIAN INSTITUTE OF INFORMATION TECHNOLOGY BHOPAL (IIIT-Bhopal) is newly established Educational Institute by the Ministry of Education, Government of India and few industry partners as Not-for-profit Public Private Partnership (N-PPP) Institution. The industry Partner of IIIT Bhopal is, Rajiv Gandhi Proudyogiki Vishwavidyalaya (RGPV, Bhopal M.P.). The capital cost of IIIT Bhopal to be contributed in the ratio of 50:35:15 by the Government of India, Government of Madhya Pradesh and third partners respectively. Approximately 50 acres Land has been identified at Bhopal for setting up the institute.
+At present, IIIT-Bhopal is operating from its make shift campus at Maulana Azad National Institute of Technology (MANIT), Bhopal, till the time its new campus is made operational at Bhopal, Madhya Pradesh. Governing Council consisting of distinguished people from academia, industry and government preside over the governance of the Institute. </p>
+    </div>
   )
 }
 
